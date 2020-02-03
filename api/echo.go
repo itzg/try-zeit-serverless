4dcaf2da4fb8a85fbbb8f10815c0374692aff95f
Geoff Bourne <itzgeoff@gmail.com>
2020-02-02 20:34:24 -0600

    Changed to echo query parameters

--- api/echo.go
+++ api/echo.go
@@ -1,12 +1,18 @@
 package api
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	
-	w.Write([]byte(r.URL.Path))
+
+	for k, vals := range r.URL.Query() {
+		fmt.Fprintf(w, "%s = %s\n", k, strings.Join(vals, ","))
+	}
 }
